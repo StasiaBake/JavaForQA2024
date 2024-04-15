935a81fafadf80192284822e2af94c653a1882b7
StasiaBake <p3rs0na99@gmail.com>
2024-04-15 14:21:56 +0400

    Update tests

--- src/main/java/ru/shop/Main.java
+++ src/main/java/ru/shop/Main.java
@@ -1,5 +1,6 @@
 package ru.shop;
 
+import ru.shop.exceptions.BadCountException;
 import ru.shop.model.Customer;
 import ru.shop.model.Order;
 import ru.shop.model.Product;
@@ -7,6 +8,9 @@ import ru.shop.model.ProductType;
 import ru.shop.repository.CustomerRepository;
 import ru.shop.repository.OrderRepository;
 import ru.shop.repository.ProductRepository;
+import ru.shop.service.CustomerService;
+import ru.shop.service.OrderService;
+import ru.shop.service.ProductService;
 
 import java.util.UUID;
 
@@ -15,71 +19,67 @@ public class Main {
     private static final CustomerRepository customerRepository = new CustomerRepository();
     private static final OrderRepository orderRepository = new OrderRepository();
 
+    private static final ProductService productService = new ProductService(productRepository);
+        private static final CustomerService customerService = new CustomerService(customerRepository);
+        private static final OrderService orderService = new OrderService(orderRepository);
+
     private static final String CONST = "CONST";
     private static final long LONG_CONST = 99999999999L;
 
     public static void main(String[] args) {
-            var product = new Product();
+        var product = new Product();
 
-            product.setId(UUID.randomUUID());
-            product.setName("product1");
-            product.setCost(99);
-            product.setProductType(ProductType.GOOD);
+        product.setId(UUID.randomUUID());
+        product.setName("product1");
+        product.setCost(99);
+        product.setProductType(ProductType.SERVICE);
 
-            System.out.println("product = " + product);
+        var product2 = new Product();
 
-            var product2 = new Product();
+        product2.setId(UUID.randomUUID());
+        product2.setName("product2");
+        product2.setCost(55);
+        product2.setProductType(ProductType.GOOD);
 
-            product2.setId(UUID.randomUUID());
-            product2.setName("product2");
-            product2.setCost(55);
-            product2.setProductType(ProductType.GOOD);
+        productService.save(product);
+        productService.save(product2);
 
-            System.out.println("product2 = " + product2);
+        var allProducts = productService.findAll();
 
-            productRepository.save(product);
-            productRepository.save(product2);
+        System.out.println("products = " + allProducts);
 
-            var allProducts = productRepository.findAll();
+        var customer = new Customer();
+        customer.setId(UUID.randomUUID());
+        customer.setName("customer1");
+        customer.setAge(33);
+        customer.setPhone("89997776655");
+        customerService.save(customer);
 
-            System.out.println("products = " + allProducts);
+        var customer2 = new Customer();
+        customer2.setId(UUID.randomUUID());
+        customer2.setName("customer2");
+        customer2.setAge(19);
+        customer2.setPhone("89997776655");
+        customerService.save(customer2);
 
-            var customer = new Customer();
-            customer.setId(UUID.randomUUID());
-            customer.setName("customer1");
-            customer.setAge(33);
-            customer.setPhone("89997776655");
-            customerRepository.save(customer);
+        System.out.println("customers = " + customerService.findAll());
 
-            var customer2 = new Customer();
-            customer2.setId(UUID.randomUUID());
-            customer2.setName("customer2");
-            customer2.setAge(19);
-            customer2.setPhone("89997776655");
-            customerRepository.save(customer2);
+        orderService.add(customer, product, 1);
+        orderService.add(customer2, product2, 1);
 
-            System.out.println("customers = " + customerRepository.findAll());
+        try {
+            orderService.add(customer, product2, -5);
+        } catch (BadCountException bce) {
+            System.out.println("Произошла ошибка: " + bce.getMessage());
+        }
 
-            var order = new Order();
-
-            order.setId(UUID.randomUUID());
-            order.setCustomerId(customer.getId());
-            order.setProductId(product.getId());
-            order.setCount(1);
-            order.setAmount(1000);
-            orderRepository.save(order);
-
-            var order2 = new Order();
-
-            order2.setId(UUID.randomUUID());
-            order2.setCustomerId(customer2.getId());
-            order2.setProductId(product2.getId());
-            order2.setCount(1);
-            order2.setAmount(500);
-            orderRepository.save(order2);
-
-            System.out.println("orders = " + orderRepository.findAll());
-            System.out.println("orders for customer = " + orderRepository.findCustomerOrders(customer.getId()));
+        System.out.println("customer count: " + customerService.findAll().size());
+        System.out.println("total orders count: " + orderService.findAll().size());
+        System.out.println("total products count: " + productService.findAll().size());
+        System.out.println("products with type GOOD: " + productService.findByProductType(ProductType.GOOD));
+        System.out.println("products with type SERVICE: " + productService.findByProductType(ProductType.SERVICE));
+        System.out.println("orders count for customer 1: " + orderService.findByCustomer(customer).size());
+        System.out.println("orders count for customer 2: " + orderService.findByCustomer(customer2).size());
     }
 
 }
